34f2cb6ed7a7142f77041a8b7098c7404d0bb153
Jurph <j.r.parsons@gmail.com>
2018-12-07 22:48:26 -0500

    Stuck on 'bool object is not callable' when I ask for SquareInch(x, y).isdisputed()

--- day3/day3p1.py
+++ day3/day3p1.py
@@ -15,23 +15,35 @@ class Claim:
         self.height = int(height)
         self.area = int(width) * int(height)
 
+    @classmethod
+    def create_claim_from_string(cls, inputstring):
+        # Could I just refactor Claim() to take an inputstring and self-initialize?
+        hashnum, at, coords, size = inputstring.split(" ")
+        number = int(hashnum[1:])
+        x, y = coords.split(",")
+        y = int(y[:-1])
+        width, height = size.split("x")
+        new_claim = cls(number, x, y, width, height)
+        return new_claim
+
 
 class SquareInch:
     """A square inch of the fabric the elves want to claim"""
-    def __init__(self, xcoordinate, ycoordinate, isclaimed=None):
+    def __init__(self, xcoordinate, ycoordinate, starting_claim=None):
         self.x = int(xcoordinate)
         self.y = int(ycoordinate)
-        if isclaimed is None or isclaimed is False:
+        if not starting_claim:
             self.isclaimed = int(0)
         else:
-            self.isclaimed = bool(isclaimed)
-        if isclaimed > 1:
+            self.isclaimed = int(starting_claim)
+        if self.isclaimed > 1:
             self.isdisputed = True
-        elif isclaimed == 1 | isclaimed == 0:
+        elif (self.isclaimed == 1) | (self.isclaimed == 0):
             self.isdisputed = False
         else:
-            print("WARN: Tenuous claim on square at {},{}".format(self.x, self.y))
+            print("WARN: Tenuous claim on square at {},{} - claim value purports to be {}".format(self.x, self.y, self.isclaimed))
             self.isdisputed = None
+        print("Created a SquareInch at ({},{}) with a claim of {} and dispute of {}".format(self.x, self.y, self.isclaimed, self.isdisputed))
 
     def claim(self):
         self.isclaimed += 1
@@ -56,7 +68,7 @@ class SquareInch:
             print("WARN: relinquishing this claim has created problems at {}, {}".format(self.x, self.y))
 
 
-def getinputarray(userfile=None):
+def get_input_array(userfile=None):
     if userfile == None:
         filename = 'input.txt'
     else:
@@ -70,23 +82,31 @@ def getinputarray(userfile=None):
     return inputs
 
 
-def parseclaim(inputstring):
-    # Could I just refactor Claim() to take an inputstring and self-initialize?
-    hashnum, at, coords, size = inputstring.split(" ")
-    number = int(hashnum[1:])
-    x, y = coords.split(",")
-    y = int(y[:-1])
-    width, height = size.split("x")
-    return number, x, y, width, height
-
-
 def main():
-    inputs = getinputarray('input.txt')
-    claims = []
-    for i in range(1, len(inputs)+1):
-        a, b, c, d, e = parseclaim(str(inputs[i]))
-        claims.append(Claim(a, b, c, d, e))
-    print("Got many claims including {}".format(claims[420]))
+    inputs = get_input_array('input.txt')
+    claims_array = []
+    for i in range(0, len(inputs)+1):
+        print("i = {} // inputs[i] = {}".format(i, inputs[i-1]))
+        claims_array.append(Claim.create_claim_from_string(str(inputs[i - 1])))
+    print("Got many claims including {}".format(claims_array[420].name))
+    for this_claim in claims_array:
+        for x in range(this_claim.x, this_claim.x + this_claim.width):
+            for y in range(this_claim.y, this_claim.y + this_claim.height):
+                this_square = SquareInch(x, y)
+                this_square.claim()
+    disputes = 0
+
+    for x in range(0, 2000):
+        for y in range(0, 2000):
+            aproblem = SquareInch(x, y).isdisputed()
+            if aproblem:
+                print("[{},{}] - Disputed with {} claims".format(x, y, SquareInch(x, y).isclaimed))
+                disputes += 1
+            elif not aproblem:
+                print("[{},{}] - Available with {} claims".format(x, y, SquareInch(x, y).isclaimed))
+            else:
+                print("[{},{}] - something strange going on here.".format(x, y))
+    print("Found {} disputes".format(disputes))
 
 
 if __name__ == "__main__":
