e3e550c7aa185c74bf348d977607e4f7bbbb9709
Jurph <j.r.parsons@gmail.com>
2018-12-07 19:29:27 -0500

    Working on turning input strings into representatives of a class.

--- day3/day3p1.py
+++ day3/day3p1.py
@@ -4,19 +4,6 @@
 # Input looks like:     #1 @ 258,327: 19x22
 
 
-def getinputarray(userfile=None):
-    if userfile == None:
-        filename = 'input.txt'
-    else:
-        filename = userfile
-    inputs = []
-    with open(filename, "r") as inputfile:
-        for lines in inputfile:
-            inputs.append(lines.rstrip())
-    inputfile.close()
-    return inputs
-
-
 class Claim:
     """An object for storing a single line of the input"""
     def __init__(self, ordinal, xcoordinate, ycoordinate, width, height):
@@ -26,7 +13,7 @@ class Claim:
         self.y = int(ycoordinate)
         self.width = int(width)
         self.height = int(height)
-        self.area = width * height
+        self.area = int(width) * int(height)
 
 
 class SquareInch:
@@ -68,3 +55,39 @@ class SquareInch:
         else:
             print("WARN: relinquishing this claim has created problems at {}, {}".format(self.x, self.y))
 
+
+def getinputarray(userfile=None):
+    if userfile == None:
+        filename = 'input.txt'
+    else:
+        filename = userfile
+    inputs = []
+    with open(filename, "r") as inputfile:
+        for lines in inputfile:
+            inputs.append(lines.rstrip())
+    inputfile.close()
+    print("Got {} input elements from {}".format(len(inputs), filename))
+    return inputs
+
+
+def parseclaim(inputstring):
+    # Could I just refactor Claim() to take an inputstring and self-initialize?
+    hashnum, at, coords, size = inputstring.split(" ")
+    number = int(hashnum[1:])
+    x, y = coords.split(",")
+    y = int(y[:-1])
+    width, height = size.split("x")
+    return number, x, y, width, height
+
+
+def main():
+    inputs = getinputarray('input.txt')
+    claims = []
+    for i in range(1, len(inputs)+1):
+        a, b, c, d, e = parseclaim(str(inputs[i]))
+        claims.append(Claim(a, b, c, d, e))
+    print("Got many claims including {}".format(claims[420]))
+
+
+if __name__ == "__main__":
+    main()
