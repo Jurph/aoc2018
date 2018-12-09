366da614aa9d1e425e1b8e6b6da83219d7b78d4f
Jurph <j.r.parsons@gmail.com>
2018-12-09 09:32:54 -0500

    Sketched in classes for input parsing

--- day4/day4.py
+++ day4/day4.py
@@ -2,14 +2,37 @@
 # Solves Day 4 of Advent of Code 2018
 # Details at https://adventofcode.com/2018/day/4
 # Input looks like:
-# [1518-11-01 00:00] Guard #10 begins shift
-# [1518-11-01 00:05] falls asleep
-# [1518-11-01 00:25] wakes up
-# [1518-11-01 00:30] falls asleep
-# [1518-11-01 00:55] wakes up
-# [1518-11-01 23:58] Guard #99 begins shift
-# [1518-11-02 00:40] falls asleep
+# [1518-09-05 23:59] Guard #79 begins shift
+# [1518-04-15 00:27] wakes up
+# [1518-03-21 00:03] Guard #1951 begins shift
+# [1518-09-29 00:45] wakes up
+# [1518-06-22 00:00] Guard #479 begins shift
+# [1518-09-22 00:12] falls asleep
+# [1518-11-11 00:39] wakes up
+# [1518-06-18 00:01] Guard #79 begins shift
 import unittest
+import datetime
+
+class TimeCard:
+    def __init__(self, employee: int, main_DTG: datetime):
+        self.guardnumber = employee
+        self.year_month_day = main_DTG.date
+        self.time_of_day = main_DTG.time
+        self.events = {
+            (self.year_month_day, self.time_of_day): "START"
+        }
+
+    @classmethod
+    def initialize_timecard_from_guard_string(cls, inputstring):
+        chunk1, chunk2 = inputstring.split("]")
+        main_DTG = datetime.fromtimestamp(chunk1[1:])  # Requires python 3.7 or newer
+        if "Guard" in chunk2:
+            employee = int(chunk2[8:].split(" "))
+        else:
+            print("Got input string without a Guard #")
+            employee = 0
+        new_timecard = cls(employee, main_DTG)
+        return new_timecard
 
 
 def get_input_array(userfile=None):
@@ -23,13 +46,11 @@ def get_input_array(userfile=None):
             inputs.append(lines.rstrip())
     inputfile.close()
     print("Got {} input elements from {}".format(len(inputs), filename))
-    return inputs
+    return inputs.sort()
 
 
 def main():
     inputs = get_input_array('input.txt')
-
-
 
 
 
