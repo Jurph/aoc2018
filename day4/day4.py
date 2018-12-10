09c9fd8967c05a0140f373c7d751d49d199f4e90
Jurph <j.r.parsons@gmail.com>
2018-12-09 20:16:30 -0500

    Fiddling around - a bit too complex I think

--- day4/day4.py
+++ day4/day4.py
@@ -13,10 +13,12 @@
 import unittest
 import datetime
 
+
 class TimeCard:
     def __init__(self, employee: int, main_DTG: datetime):
         self.employee = employee
         self.year_month_day = main_DTG.date
+        (self.year, self.month, self.day, self.hour, self.minute, self.second, self.weekday, self.yday, self.dst) = main_DTG.timetuple()
         self.time_of_day = main_DTG.time
         self.events = {
             (self.year_month_day, self.time_of_day): "START"
@@ -24,16 +26,35 @@ class TimeCard:
 
     @classmethod
     def initialize_timecard_from_guard_string(cls, inputstring):
-        chunk1, chunk2 = inputstring.split("]")
-        main_DTG = chunk1[1:].fromisoformat  # Requires python 3.7 or newer
-        if "Guard" in chunk2:
-            employee = int(chunk2[8:].split(" "))
+        event_text, shift_began_at = cls.parse_input_string(inputstring)
+        if "Guard" in event_text:
+            employee = int(str(event_text[8:].split(" ")[0]))
         else:
             print("Got input string without a Guard #")
-            employee = 0
-        new_timecard = cls(employee, main_DTG)
+            employee = None
+        new_timecard = cls(employee, shift_began_at)
         return new_timecard
 
+    def parse_input_string(self, inputstring):
+        raw_time_string, event_text = inputstring.split("]")
+        raw_time_string = str(raw_time_string[1:])
+        event_datetime = datetime.datetime.fromisoformat(raw_time_string)  # Requires python 3.7 or newer
+        return event_text, event_datetime
+
+    @classmethod
+    def add_a_nap_from_input_strings(cls, inputstring1, inputstring2):
+        start_event_text, nap_started_at = inputstring1.parse_input_string()
+        end_event_text, nap_ended_at = inputstring2.parse_input_string()
+        if ("falls asleep" in start_event_text) and ("wakes up" in end_event_text):
+
+            return nap_started_at, nap_ended_at
+        elif "falls asleep" not in start_event_text:
+            print("No start event in {}".format(inputstring1))
+        elif "wakes up" not in end_event_text:
+            print("No end event in {}".format(inputstring2))
+        else:
+            print("Well this is awkward.")
+            quit(1)
 
 def get_input_array(userfile=None):
     if userfile == None:
@@ -44,13 +65,16 @@ def get_input_array(userfile=None):
     with open(filename, "r") as inputfile:
         for lines in inputfile:
             inputs.append(lines.rstrip())
-    inputfile.close()
     print("Got {} input elements from {}".format(len(inputs), filename))
-    return inputs.sort()
+    inputs.sort()
+    return inputs
 
 
 def main():
+    inputs = []
     inputs = get_input_array('input.txt')
+    for i in range(20):
+        print(inputs[i])
 
 
 class TestCustomFunctions(unittest.TestCase):
@@ -59,11 +83,31 @@ class TestCustomFunctions(unittest.TestCase):
         init_string = "[1518-09-05 23:59] Guard #79 begins shift"
         card = TimeCard.initialize_timecard_from_guard_string(init_string)
         self.assertEqual(card.employee, 79)
-        self.assertEqual(card.year_month_day.year(), 1518)
-        self.assertEqual(card.year_month_day.month(), 9)
-        self.assertEqual(card.year_month_day.day(), 5)
+        self.assertEqual(card.year, 1518)
+        self.assertEqual(card.month, 9)
+        self.assertEqual(card.day, 5)
+
+    def test_init_safe_failure(self):
+        init_string = "[1518-04-15 00:27] wakes up"
+        card = TimeCard.initialize_timecard_from_guard_string(init_string)
+        self.assertEqual(card.employee, None)
+        self.assertEqual(card.year, 1518)
+        self.assertEqual(card.month, 4)
+        self.assertEqual(card.day, 15)
+
+    def test_nap_measurement(self):
+        inits = ["[1518-03-10 23:57] Guard #73 begins shift",
+                    "[1518-03-11 00:06] falls asleep",
+                    "[1518-03-11 00:22] wakes up",
+                    "[1518-03-11 00:38] falls asleep",
+                    "[1518-03-11 00:41] wakes up"]
+        card = TimeCard.initialize_timecard_from_guard_string(inits[1])
+        init_string1 = inits[2]
+        init_string2 = inits[3]
+        nap_start, nap_end = add_a_nap_from_input_strings(init_string1, init_string2)
+
 
 
 if __name__ == "__main__":
-    unittest.main()
     main()
+    unittest.main()
